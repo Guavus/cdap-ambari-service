baf209fe2054dddba7e98fb80ecae0bca55b84b1
Rajat Goel <rajat.goel@thalesgroup.com>
2020-07-31 13:16:01 +0530

    RAF-1570: Fixing scripts to clean up stale processes during stop as well as chek for stale processes during startup (#21)

--- src/main/resources/common-services/CDAP/5.0.0/package/scripts/auth.py
+++ src/main/resources/common-services/CDAP/5.0.0/package/scripts/auth.py
@@ -16,7 +16,7 @@
 
 import ambari_helpers as helpers
 from resource_management import *
-
+import subprocess as sp
 
 class Auth(Script):
     def install(self, env):
@@ -39,6 +39,11 @@ class Auth(Script):
         import status_params
         env.set_params(params)
         self.configure(env)
+
+        pids = self.__checkStaleProcess()
+        if pids != '':
+            raise Exception('Stale processes running on the system. First kill them using stop command and then trigger start')
+
         daemon_cmd = format('/opt/cdap/security/bin/cdap auth-server start')
         no_op_test = format('ls {status_params.cdap_auth_pid_file} >/dev/null 2>&1 && ps -p $(<{status_params.cdap_auth_pid_file}) >/dev/null 2>&1')
         Execute(
@@ -58,6 +63,9 @@ class Auth(Script):
             user=params.cdap_user,
             only_if=no_op_test
         )
+        ret = self.__killStaleProcess()
+        if ret > 0:
+            raise Exception('Stale processes could not be killed. Please kill them manually')
 
     def status(self, env):
         import status_params
@@ -68,6 +76,44 @@ class Auth(Script):
         import params
         env.set_params(params)
         helpers.cdap_config('auth')
+
+    def executeShellCommands(self, command):
+        output = ''
+        status = 1
+
+        pipe = sp.Popen(command, shell=True, stdout=sp.PIPE, stderr=sp.PIPE )
+        if pipe.wait() != 0:
+            print("Command execution fail " + command)
+            return status, output
+        output = pipe.communicate()[0].strip()
+        status = 0
+        return status, output
+
+    def __checkStaleProcess(self):
+        ps_command = "ps -ef | grep 'cdap.service=auth-server' | grep -v grep | awk '{print $2}'"
+        status, output = self.executeShellCommands(ps_command)
+        if output == '':
+            print 'No stale process'
+            return ''
+        else:
+            print 'Stale process found with pid(s): ' + output
+            return output
+
+    def __killStaleProcess(self):
+        pids = self.__checkStaleProcess()
+        if pids == '':
+            return 0
+        else:
+            pid_list = pids.splitlines()
+            for pid in pid_list:
+                kill_command = "kill -9 " + str(pid)
+                Execute(kill_command, user='cdap')
+            new_pids = self.__checkStaleProcess()
+            if new_pids != '':
+                print 'Stale process could not be killed, pid(s): ' + str(new_pids)
+                return 1
+            return 0
+
 
 if __name__ == "__main__":
     Auth().execute()
